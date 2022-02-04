3608872cd9e6d3e4d23d2c412292c238e4d2fd06
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-02-03 23:37:08 -0600

    about and headshot

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ function About(props) {
                     {parseIntro(data.about)}
                   </Col>
                   <Col style={styles.introImageContainer}>
-                    <img src={data?.imageSource} alt="profile" />
+                    <img src={data?.imageSource} alt="profile" height="300" />
                   </Col>
                 </Row>
               </Fade>
